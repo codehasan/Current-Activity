b87df36db373c349cedb4ff820472dc4fb85e898
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2026-03-20 10:46:11 +0600

    refactor autostart utility

--- app/src/main/java/io/github/ratul/topactivity/utils/AutostartUtil.kt
+++ app/src/main/java/io/github/ratul/topactivity/utils/AutostartUtil.kt
@@ -58,10 +58,18 @@ object AutostartUtil {
             brand.containsAny("transsion", "tecno", "infinix", "itel") -> tecnoInfinixIntents()
             brand.contains("samsung") -> samsungIntents()
             else -> arrayOf(
-                classIntent("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity"),
-                Intent(Intent.ACTION_MAIN)
-                    .setComponent(ComponentName("com.android.settings", "com.android.settings.Settings\$AccessLockSummaryActivity"))
-                    .putExtra("packageName", context.packageName)
+                classIntent(
+                    "com.letv.android.letvsafe",
+                    "com.letv.android.letvsafe.AutobootManageActivity"
+                ),
+                Intent(Intent.ACTION_MAIN).apply {
+                    component =
+                        ComponentName(
+                            "com.android.settings",
+                            $$"com.android.settings.Settings$AccessLockSummaryActivity"
+                        )
+                    putExtra("packageName", context.packageName)
+                }
             )
         }
     }
@@ -70,7 +78,10 @@ object AutostartUtil {
         keywords.any { contains(it) }
 
     private fun xiaomiIntents() = arrayOf(
-        classIntent("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity"),
+        classIntent(
+            "com.miui.securitycenter",
+            "com.miui.permcenter.autostart.AutoStartManagementActivity"
+        ),
         actionIntent("miui.intent.action.OP_AUTO_START"),
     )
 
@@ -80,41 +91,95 @@ object AutostartUtil {
     )
 
     private fun oppoRealmeOnePlusIntents() = arrayOf(
-        classIntent("com.coloros.safecenter", "com.coloros.safecenter.startupapp.StartupAppListActivity"),
-        classIntent("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.StartupAppListActivity"),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.safecenter.startupapp.StartupAppListActivity"
+        ),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.safecenter.permission.startup.StartupAppListActivity"
+        ),
         classIntent("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity"),
-        classIntent("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.FakeActivity"),
-        classIntent("com.coloros.safecenter", "com.coloros.safecenter.permission.startupapp.StartupAppListActivity"),
-        classIntent("com.coloros.safecenter", "com.coloros.safecenter.permission.startupmanager.StartupAppListActivity"),
-        classIntent("com.coloros.safe", "com.coloros.safe.permission.startup.StartupAppListActivity"),
-        classIntent("com.coloros.safe", "com.coloros.safe.permission.startupapp.StartupAppListActivity"),
-        classIntent("com.coloros.safe", "com.coloros.safe.permission.startupmanager.StartupAppListActivity"),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.safecenter.permission.startup.FakeActivity"
+        ),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.safecenter.permission.startupapp.StartupAppListActivity"
+        ),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.safecenter.permission.startupmanager.StartupAppListActivity"
+        ),
+        classIntent(
+            "com.coloros.safe",
+            "com.coloros.safe.permission.startup.StartupAppListActivity"
+        ),
+        classIntent(
+            "com.coloros.safe",
+            "com.coloros.safe.permission.startupapp.StartupAppListActivity"
+        ),
+        classIntent(
+            "com.coloros.safe",
+            "com.coloros.safe.permission.startupmanager.StartupAppListActivity"
+        ),
         classIntent("com.coloros.safecenter", "com.coloros.safecenter.permission.startsettings"),
-        classIntent("com.coloros.safecenter", "com.coloros.safecenter.permission.startupapp.startupmanager"),
-        classIntent("com.coloros.safecenter", "com.coloros.safecenter.permission.startupmanager.startupActivity"),
-        classIntent("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.startupapp.startupmanager"),
-        classIntent("com.coloros.safecenter", "com.coloros.privacypermissionsentry.PermissionTopActivity.Startupmanager"),
-        classIntent("com.coloros.safecenter", "com.coloros.privacypermissionsentry.PermissionTopActivity"),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.safecenter.permission.startupapp.startupmanager"
+        ),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.safecenter.permission.startupmanager.startupActivity"
+        ),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.safecenter.permission.startup.startupapp.startupmanager"
+        ),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.privacypermissionsentry.PermissionTopActivity.Startupmanager"
+        ),
+        classIntent(
+            "com.coloros.safecenter",
+            "com.coloros.privacypermissionsentry.PermissionTopActivity"
+        ),
         classIntent("com.coloros.safecenter", "com.coloros.safecenter.FakeActivity"),
     )
 
     private fun vivoIntents() = arrayOf(
-        classIntent("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity"),
+        classIntent(
+            "com.vivo.permissionmanager",
+            "com.vivo.permissionmanager.activity.BgStartUpManagerActivity"
+        ),
         classIntent("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager"),
         classIntent("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity"),
         actionIntent("com.iqoo.secure.BGSTARTUPMANAGER"),
     )
 
     private fun huaweiHonorIntents() = arrayOf(
-        classIntent("com.huawei.systemmanager", "com.huawei.systemmanager.startupmgr.ui.StartupNormalAppListActivity"),
-        classIntent("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity"),
-        classIntent("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.bootstart.BootStartActivity"),
+        classIntent(
+            "com.huawei.systemmanager",
+            "com.huawei.systemmanager.startupmgr.ui.StartupNormalAppListActivity"
+        ),
+        classIntent(
+            "com.huawei.systemmanager",
+            "com.huawei.systemmanager.optimize.process.ProtectActivity"
+        ),
+        classIntent(
+            "com.huawei.systemmanager",
+            "com.huawei.systemmanager.optimize.bootstart.BootStartActivity"
+        ),
         actionIntent("huawei.intent.action.HSM_STARTUPAPP_MANAGER"),
         actionIntent("huawei.intent.action.HSM_BOOTAPP_MANAGER"),
     )
 
     private fun samsungIntents() = arrayOf(
-        classIntent("com.samsung.android.lool", "com.samsung.android.sm.ui.battery.BatteryActivity"),
+        classIntent(
+            "com.samsung.android.lool",
+            "com.samsung.android.sm.ui.battery.BatteryActivity"
+        ),
         classIntent("com.samsung.android.sm", "com.samsung.android.sm.ui.battery.BatteryActivity"),
         classIntent("com.samsung.android.sm_cn", "com.samsung.android.sm.ui.ram.AutoRunActivity"),
         actionIntent("com.samsung.android.sm.ACTION_BATTERY"),
